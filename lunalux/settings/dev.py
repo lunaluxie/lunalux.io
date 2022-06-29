b490a202aa6cde78145b0aeed2d0191fa71c2219
Kasper Fredenslund <kasper@kasperfred.com>
2022-06-29 13:18:22 +0200

    create wagtail cookiecutter

---
+++ lunalux/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-waw-&0h%wa96+60qmg00%zrsz1()ro2t9i4k9)f)yzs1(5&f2%"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass

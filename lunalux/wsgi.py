38dfe671ba8cf660715a9e2ad2f193ea6024d7ba
Luna Lux Fredenslund <luna@lunalux.io>
2024-04-01 09:56:47 +0200

    Update wsgi.py to use production settings

--- lunalux/wsgi.py
+++ lunalux/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lunalux.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lunalux.settings.production")
 
 application = get_wsgi_application()

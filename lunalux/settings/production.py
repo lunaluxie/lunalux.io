65ac8bfd7cea788b65957db2ab2a522169bab49a
Luna Fredenslund <luna@lunalux.io>
2024-04-14 13:03:36 +0200

    update static files to seperate server to serve static files seperately.

--- lunalux/settings/production.py
+++ lunalux/settings/production.py
@@ -23,7 +23,7 @@ DATABASES = {
 
 
 # change these to fit.
-root = "/home/kasfre2/lunalux.io/public"
+root = "/home/kasfre2/cdn.lunalux.io/public"
 STATIC_ROOT = os.path.join(root, "static")
 MEDIA_ROOT = os.path.join(root, "media")
 

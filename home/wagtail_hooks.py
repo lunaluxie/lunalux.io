3047d448aab4ea2428a3662e01c1e35c386f1371
Luna Fredenslund <luna@lunalux.io>
2024-06-26 21:40:01 +0200

    add contact to admin menu again

--- home/wagtail_hooks.py
+++ home/wagtail_hooks.py
@@ -10,9 +10,10 @@ from home.models.helper_models import InterPageLink, Contact, PageHit
 class ContactAdmin(SnippetViewSet):
     model = Contact
     menu_label = 'Contact'  # ditch this to use verbose_name_plural from model
-    menu_icon = 'pilcrow'  # change as required
+    menu_name = 'contact'
+    icon = 'pilcrow'  # change as required
     menu_order = 200  # will put in 3rd place (000 being 1st, 100 2nd)
-    add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
+    add_to_admin_menu = True  # or True to add your model to the Settings sub-menu
 
     list_display = ('email', 'message', 'name', 'timestamp')
     list_filter = ('email','timestamp')
